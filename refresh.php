068678b0abd6b14c83e1932e24793cdfa61541c5
Nitin Kumar <himanshunagpal25061992@gmail.com>
2019-12-18 14:08:01 +0530

    PUSH

--- refresh.php
+++ refresh.php
@@ -2,6 +2,9 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+error_reporting(E_ALL);
+ini_set('display_errrors',2);
+
 
 use QuickBooksOnline\API\Core\ServiceContext;
 use QuickBooksOnline\API\DataService\DataService;
