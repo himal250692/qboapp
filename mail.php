5d65425f3817242435db15ecce4f6b547e6ec303
himanshuitians <himanshuitians@gmail.com>
2020-01-03 18:17:51 +0530

    Push mail

--- mail.php
+++ mail.php
@@ -26,12 +26,21 @@ define('DB_NAME',$db);
 
 $connection = new Sql();
 $connectStr = $connection->connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$request = json_encode($_REQUEST);
 
+                
 $CommonFunction = new CommonFunction($connectStr);
+
+
+$insertData = array();
+$insertData['log'] = $request;
+$CommonFunction->insertData('logs',$insertData);
+
+
 $logs = $CommonFunction->Query("Select * from logs");
 echo "<pre/>"; print_r($logs); 
 
-die;
+
 
 class CommonFunction{
 	public $connectStr;
@@ -174,6 +183,78 @@ class CommonFunction{
 	}
 	
 }
+Class Sql{	
+	var $username;
+	var $password;
+	var $database;
+	public $dbc;
 
+	public function connect($set_host, $set_username, $set_password, $set_database)
+	{
+		$this->host = $set_host;
+		$this->username = $set_username;
+		$this->password = $set_password;
+		$this->database = $set_database;
+
+		$this->dbc = mysqli_connect($this->host, $this->username, $this->password,$this->database) or die('Error connecting to DB'); 
+		return $this->dbc;
+	}
+	
+	public function query($sql)
+	{
+		return mysqli_query($this->dbc, $sql) or false;
+	}
+	
+	public function fetch($result)
+	{        
+		$array = mysqli_fetch_array($result);          
+		return $array;
+	}
+
+
+	public function close()
+	{
+		return mysqli_close($this->dbc);
+	}
+	public function login($email,$password)
+	{
+		
+		$response = array();
+		if($email  == 'admin' && $password == md5('Admin@123$')){
+			$response['status'] = true;
+			$response['result'] = array('user_id'=>1,'name'=>'Admin');
+		} else {
+			$response['status'] = false;
+			$response['errortype'] = 'wrong_cred';
+			$response['message'] = 'User does not exist in our system.';
+		}
+		return $response; 
+		
+	}
+	public function getcompany($clientId)
+	{
+		$sql = "Select * from qbap_company where client_id='$clientId'";
+		$result = mysqli_query($this->dbc, $sql);
+		$response = array();
+		if($result){
+			
+			if(mysqli_num_rows($result) > 0){
+				$response['status'] = true;
+				while($row = $result->fetch_assoc()) {
+					$newResponse[] = $row;
+				}
+				$response['result'] = $newResponse;				
+			} 
+			
+		} else {
+			$response['status'] = false;
+			$response['errortype'] = 'db_error';
+			$response['message'] =  $this->dbc->error;
+		}
+		
+		return $response; 
+		
+	}
+}
 
 ?>
